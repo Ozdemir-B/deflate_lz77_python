85448811a648ede9b1f37968da2f4d59edfd181e
Ozdemir-B <60180817+Ozdemir-B@users.noreply.github.com>
2020-05-18 17:08:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from skalg import *
 
 lz=lz77().encode("metin.txt",300,100)
 #encode("file.txt",search buffer size, look ahead buffer size)
-#you need to decompress the file_encoded file
-#file_encoded_txt.txt file shows the compressed file
+#file_encoded.txt is the compressed file
 #returns an array of tuples [(offset,lenght,character)]
 
 
 lz77().decode("metin_encoded.txt")
-#decode("file_encoded") creates file_encoded_decoded.txt which is the reconstructed text
+#decode("file_encoded.txt") creates file_encoded_decoded.txt which is the reconstructed text
 
 
 
@@ -23,5 +22,5 @@ deflate().encode("metin.txt",300,100)
 deflate().decode("metin_deflated.txt","metin_key_o.txt","metin_key_s.txt")
 #decode("file_deflated.txt","file_key.txt")
 #uses file_key_o.txt and file_key_s.txt as the key for huffman decoding
-#creates file_deflated_unhuffed as the reconstructed lz77 form of the file
+#creates file_deflated_unhuffed.txt as the reconstructed lz77 form of the file
 #creates file_deflated_unhuffed_encoded.txt as the reconstructed file
